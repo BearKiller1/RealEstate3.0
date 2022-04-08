d5d4cf036f02ea7ba5514d87b5416f03b91ce3a7
BearKiller22 <dachi.xucishvili53@gmail.com>
2022-04-08 14:21:15 +0400

    search

---
+++ components/search/search.js
@@ -0,0 +1,51 @@
+$(document).ready(() => {
+    GetProduct();
+})
+
+GetProduct = () => {
+    var sort;
+
+    if($("#sort").val() == "oldest")
+        sort = "ORDER BY id ASC";
+    else if($("#sort").val() == "latest")
+        sort = "ORDER BY id DESC";
+    else if($("#sort").val() == "cheapest")
+        sort = "ORDER BY price ASC";
+    else if($("#sort").val() == "most expensive")
+        sort = "ORDER BY price DESC";
+    else
+        sort = "ORDER BY id";
+
+    var data = {
+        title   : $("#searchbox123").val(),
+        sorting : $("#sorting").val(),
+        price   : $("#price").val(),
+        area    : $("#area").val(),
+
+        min_price   :   $("#ipmin").val(),
+        max_price   :   $("#ipmax").val(),
+
+        min_area    :   $("#iAmin").val(),
+        max_area    :   $("#iAmax").val(),
+
+        sorting     :   sort,
+    }
+    Ajax({
+        url:"search",
+        object:{
+            method:"GetProduct",
+            data
+        },
+        type:"POST",
+        success: (response) => {
+            response = JSON.parse(response);
+            
+            if(response.page != ""){
+                $("#prod_container").html(response.page)
+            }
+            else{
+                $("#prod_container").html("<h1>No results found</h1>");
+            }
+        }
+    })
+}
--- components/search/search.php
+++ components/search/search.php
@@ -2,6 +2,7 @@
 <?php
     
     include_once "../../includes/Lucid.class.php";
+    require_once  "../Common.class.php";
 
     class Search extends Lucid{
         public $response;
@@ -11,9 +12,74 @@
          * This constructor Sets up response request and connection
          */
         public function __construct(){
+            $this->common = new Common();
+
             $this->response = array();
             $this->request  = $_REQUEST["data"];
+            $this->priceFilter;
+            $this->areaFilter;
+            $this->titleFilter;
             Parent::__construct();
+        }
+
+        public function GetProduct(){
+            $this->FilterBuilder();
+
+            $this->imageSql = "SELECT * FROM files WHERE product_id = ?";
+            
+            $this->sql = "  SELECT  products.id AS product_id,
+                                    products.*,
+                                    users.username,
+                                    users.phone 
+                            FROM products 
+                            LEFT JOIN files     ON products.id = files.product_id
+                            LEFT JOIN users     ON products.user_id = users.id AND users.actived = 1
+                            LEFT JOIN `condition` ON products.condition_id = `condition`.id
+                            WHERE products.actived = 1 ".$this->priceFilter." ".$this->areaFilter." ".$this->titleFilter."
+                            GROUP BY products.id
+                            ".$this->request['sorting'];
+
+            $this->response = Parent::GetData($this->sql, []);
+
+            for ($i=0; $i < COUNT($this->response); $i++) {
+                $this->response[$i]['images'] = Parent::GetData($this->imageSql, [$this->response[$i]['product_id']]);
+            }
+            $method = '';
+            $this->response['page'] = $this->common->CreateProductHTML($this->response);
+        }
+
+        public function FilterBuilder(){
+            $this->min = $this->request['min_price'];
+            $this->max = $this->request['max_price'];
+
+            if($this->min != ''){
+                $this->priceFilter = " AND price >= ".$this->min;
+            }
+            if($this->max != ''){
+                $this->priceFilter .= " AND price <= ".$this->max;
+            }
+
+            $this->min = $this->request['min_area'];
+            $this->max = $this->request['max_area'];
+            
+            if($this->min != ''){
+                $this->areaFilter = " AND area >= ".$this->min;
+            }
+            if($this->max != ''){
+                $this->areaFilter .= " AND area <= ".$this->max;
+            }
+
+            $title = $this->request['title'];
+
+            if($title != ''){
+                $this->titleFilter = " 
+                    AND title LIKE '%".$title."%' OR title LIKE '% ".$title." ' OR title LIKE '".$title."%'
+                OR  description_en LIKE '%".$title."%' OR description_en LIKE '% ".$title." ' OR description_en LIKE '".$title."%'
+                OR  address LIKE '%".$title."%' OR address LIKE '% ".$title." ' OR address LIKE '".$title."%'
+                OR  description LIKE '%".$title."%' OR description LIKE '% ".$title." ' OR description LIKE '".$title."%'    
+                ";
+            }
+
         }
 
         /**
