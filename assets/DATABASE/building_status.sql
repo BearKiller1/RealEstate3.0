27b0dc09a2e15dcb7fe9211663bbdd1c5c3a68ab
BearKiller22 <dachi.xucishvili53@gmail.com>
2022-05-22 12:54:09 +0400

    database scripts

---
+++ assets/DATABASE/building_status.sql
@@ -0,0 +1,3 @@
+INSERT INTO `building_status`(`id`, `name`, `actived`) VALUES (1, 'New Building', 1);
+INSERT INTO `building_status`(`id`, `name`, `actived`) VALUES (2, 'Under Construction', 1);
+INSERT INTO `building_status`(`id`, `name`, `actived`) VALUES (3, 'Old Building', 1);
--- assets/DATABASE/users.sql
+++ assets/DATABASE/users.sql
@@ -1,1 +1,2 @@
-INSERT INTO `users`(`id`, `username`, `password`, `email`, `phone`, `profile_pic`, `surname`, `show_perm`, `gender`, `actived`) VALUES (48, 'ADMIN', '202cb962ac59075b964b07152d234b70', 'Admin', '666666666', '', 'ADMIN', 0, 1, 1);
+INSERT INTO `users`(`id`, `username`, `password`, `email`, `phone`, `profile_pic`, `surname`, `show_perm`, `gender`, `actived`) VALUES (48, 'ADMIN', '202cb962ac59075b964b07152d234b70', 'Admin', '666666666', 'assets/profile/home-banner-2020-02-min.jpg', 'ywerwer', 1, 1, 1);
+INSERT INTO `users`(`id`, `username`, `password`, `email`, `phone`, `profile_pic`, `surname`, `show_perm`, `gender`, `actived`) VALUES (49, 'Dachi', '202cb962ac59075b964b07152d234b70', 'dachi', '568141665', NULL, 'Khutsishvili', 1, 1, 1);
